9802e49ad037e7bf069c45fabe19e647bfcb952d
YaroslavKSE <ikhomych@kse.org.ua>
2022-04-19 12:09:55 +0300

    Added "Karatsuba" fixed sub, added usage

--- Task_5/BigInteger.cs
+++ Task_5/BigInteger.cs
@@ -65,14 +65,18 @@ public class BigInteger
         int secondExtraLenght = another._numbers.Length + 1;
         int firstArrayLen = _numbers.Length - 1;
         int secondArrayLen = another._numbers.Length - 1;
+        int[] result = new int[_numbers.Length];
+        int[] result2 = new int[another._numbers.Length];
+        Array.Copy(_numbers, result, _numbers.Length);
+        Array.Copy(another._numbers, result2, another._numbers.Length);
         while (firstArrayLen != -1 && secondArrayLen != -1)
         {
             if (firstArrayLen >= secondArrayLen)
             {
-                var addition = _numbers[firstArrayLen] + another._numbers[secondArrayLen];
+                var addition = result[firstArrayLen] + another._numbers[secondArrayLen];
                 if (addition < 10)
                 {
-                    _numbers[firstArrayLen] = addition;
+                    result[firstArrayLen] = addition;
                 }
                 else
                 {
@@ -83,27 +87,27 @@ public class BigInteger
                     {
                         int[] temporaryArray = new int[firstExtraLenght];
                         int n = firstExtraLenght;
-                        foreach (var digit in _numbers)
+                        foreach (var digit in result)
                         {
-                            temporaryArray[n - 1] = _numbers[n - 2];
+                            temporaryArray[n - 1] = result[n - 2];
                             n--;
                         }
 
-                        _numbers = temporaryArray;
+                        result = temporaryArray;
                         firstArrayLen++;
                     }
 
-                    _numbers[firstArrayLen - 1] = Int32.Parse(first.ToString()) + _numbers[firstArrayLen - 1];
-                    _numbers[firstArrayLen] = Int32.Parse(second.ToString());
+                    result[firstArrayLen - 1] = Int32.Parse(first.ToString()) + result[firstArrayLen - 1];
+                    result[firstArrayLen] = Int32.Parse(second.ToString());
                 }
             }
 
             if (firstArrayLen < secondArrayLen)
             {
-                var addition = _numbers[firstArrayLen] + another._numbers[secondArrayLen];
+                var addition = _numbers[firstArrayLen] + result2[secondArrayLen];
                 if (addition < 10)
                 {
-                    another._numbers[secondArrayLen] = addition;
+                    result2[secondArrayLen] = addition;
                 }
                 else
                 {
@@ -114,19 +118,18 @@ public class BigInteger
                         int[] temporaryArray = new int[secondExtraLenght];
                         int n = secondExtraLenght;
 
-                        foreach (var digit in _numbers)
+                        foreach (var digit in result)
                         {
-                            temporaryArray[n - 1] = _numbers[n - 2];
+                            temporaryArray[n - 1] = result[n - 2];
                             n--;
                         }
 
-                        another._numbers = temporaryArray;
+                        result2 = temporaryArray;
                         secondExtraLenght++;
                     }
 
-                    another._numbers[secondArrayLen - 1] =
-                        Int32.Parse(first.ToString()) + another._numbers[secondArrayLen - 1];
-                    another._numbers[secondArrayLen] = Int32.Parse(second.ToString());
+                    result2[secondArrayLen - 1] = Int32.Parse(first.ToString()) + result2[secondArrayLen - 1];
+                    result2[secondArrayLen] = Int32.Parse(second.ToString());
                 }
             }
 
@@ -134,60 +137,67 @@ public class BigInteger
             secondArrayLen--;
         }
 
-        return firstArrayLen >= secondArrayLen ? new BigInteger(string.Join("", _numbers)) : another;
+        return firstArrayLen >= secondArrayLen
+            ? new BigInteger(string.Join("", result))
+            : new BigInteger(string.Join("", result2));
     }
 
     private BigInteger Sub(BigInteger another)
     {
         int firstArrayLen = _numbers.Length - 1;
         int secondArrayLen = another._numbers.Length - 1;
+        int[] result = new int[_numbers.Length];
+        int[] result2 = new int[another._numbers.Length];
+        Array.Copy(_numbers, result, _numbers.Length);
+        Array.Copy(another._numbers, result2, another._numbers.Length);
 
         if (firstArrayLen > secondArrayLen)
         {
             while (firstArrayLen != -1 && secondArrayLen != -1)
             {
                 {
-                    var subtraction = _numbers[firstArrayLen] - another._numbers[secondArrayLen];
+                    var subtraction = result[firstArrayLen] - result2[secondArrayLen];
                     if (subtraction >= 0)
                     {
-                        _numbers[firstArrayLen] = subtraction;
+                        result[firstArrayLen] = subtraction;
                     }
                     else
                     {
-                        if (_numbers[firstArrayLen] != 0)
+                        if (result[firstArrayLen] != 0)
                         {
-                            _numbers[firstArrayLen - 1] -= 1;
-                            var toChange = 10 + _numbers[firstArrayLen];
-                            toChange -= another._numbers[secondArrayLen];
-                            _numbers[firstArrayLen] = toChange;
+                            result[firstArrayLen - 1] -= 1;
+                            var toChange = 10 + result[firstArrayLen];
+                            toChange -= result2[secondArrayLen];
+                            result[firstArrayLen] = toChange;
                         }
                         else
                         {
                             while (true)
                             {
                                 var counter = 0;
-                                foreach (var variable in _numbers.Reverse())
+                                foreach (var variable in result.Reverse())
                                 {
                                     if (variable != 0)
                                     {
-                                        var toChange = 10 + _numbers[firstArrayLen];
-                                        toChange -= another._numbers[secondArrayLen];
-                                        _numbers[firstArrayLen] = toChange;
+                                        var toChange = 10 + result[firstArrayLen];
+                                        toChange -= result2[secondArrayLen];
+                                        result[firstArrayLen] = toChange;
                                     }
 
                                     counter++;
                                     if (firstArrayLen - counter < 0)
                                     {
-                                        continue;
+                                        break;
                                     }
 
-                                    if (_numbers[firstArrayLen - counter] == 0)
+                                    if (result[firstArrayLen - counter] == 0)
                                     {
-                                        _numbers[firstArrayLen - counter] = 9;
+                                        // result[firstArrayLen] = 9;
+                                        result[firstArrayLen - counter] = 9;
                                     }
                                     else
                                     {
-                                        _numbers[firstArrayLen - counter] -= 1;
+                                        result[firstArrayLen - counter] -= 1;
                                     }
                                 }
 
@@ -201,53 +211,53 @@ public class BigInteger
                 }
             }
 
-            return new BigInteger(string.Join("", _numbers));
+            return new BigInteger(string.Join("", result));
         }
 
         if (firstArrayLen == secondArrayLen && _numbers[0] > another._numbers[0])
         {
             while (firstArrayLen != -1 && secondArrayLen != -1)
             {
-                var subtraction = _numbers[firstArrayLen] - another._numbers[secondArrayLen];
+                var subtraction = result[firstArrayLen] - another._numbers[secondArrayLen];
                 if (subtraction >= 0)
                 {
-                    _numbers[firstArrayLen] = subtraction;
+                    result[firstArrayLen] = subtraction;
                 }
                 else
                 {
-                    _numbers[firstArrayLen - 1] -= 1;
-                    var toChange = 10 + _numbers[firstArrayLen];
+                    result[firstArrayLen - 1] -= 1;
+                    var toChange = 10 + result[firstArrayLen];
                     toChange -= another._numbers[secondArrayLen];
-                    _numbers[firstArrayLen] = toChange;
+                    result[firstArrayLen] = toChange;
                 }
 
                 firstArrayLen--;
                 secondArrayLen--;
             }
 
-            return new BigInteger(string.Join("", _numbers));
+            return new BigInteger(string.Join("", result));
         }
 
         if (firstArrayLen == secondArrayLen && _numbers[0] < another._numbers[0])
         {
-            var temporaryArray1 = _numbers;
-            var temporaryArray2 = another._numbers;
-            _numbers = temporaryArray2;
-            another._numbers = temporaryArray1;
+            var temporaryArray1 = result;
+            var temporaryArray2 = result2;
+            result = temporaryArray2;
+            result2 = temporaryArray1;
 
             while (firstArrayLen != -1 && secondArrayLen != -1)
             {
-                var subtraction = _numbers[firstArrayLen] - another._numbers[secondArrayLen];
+                var subtraction = result[firstArrayLen] - result2[secondArrayLen];
                 if (subtraction >= 0)
                 {
-                    _numbers[firstArrayLen] = subtraction;
+                    result[firstArrayLen] = subtraction;
                 }
                 else
                 {
-                    _numbers[firstArrayLen - 1] -= 1;
-                    var toChange = 10 + _numbers[firstArrayLen];
-                    toChange -= another._numbers[secondArrayLen];
-                    _numbers[firstArrayLen] = toChange;
+                    result[firstArrayLen - 1] -= 1;
+                    var toChange = 10 + result[firstArrayLen];
+                    toChange -= result2[secondArrayLen];
+                    result[firstArrayLen] = toChange;
                 }
 
                 firstArrayLen--;
@@ -255,24 +265,86 @@ public class BigInteger
             }
         }
 
-        var result = new BigInteger(string.Join("", _numbers))
+        var res = new BigInteger(string.Join("", result))
         {
             _isPositive = false
         };
-        return result;
+        return res;
     }
 
-    public BigInteger Multiplication(BigInteger second)
+    public BigInteger PowerNum(int toPower)
     {
-        var firstArrayLength = _numbers.Length;
-        var secondArrayLength = second._numbers.Length;
-        var a = Int32.Parse(string.Join("", _numbers[0], _numbers[1]));
-        var b = Int32.Parse(string.Join("", _numbers[2], _numbers[3]));
-        var c = Int32.Parse(string.Join("", second._numbers[0], second._numbers[1]));
-        var d = Int32.Parse(string.Join("", second._numbers[2], second._numbers[3]));
-        var x = Math.Pow(10, firstArrayLength / 2) * a + b;
-        var y = Math.Pow(10, secondArrayLength / 2) * c + d;
-        var result = x * y;
-        return new BigInteger(result.ToString());
+        var arrLen = _numbers.Length;
+        var secondLen = arrLen + toPower;
+        int[] result = new int[secondLen];
+        Array.Copy(_numbers, result, _numbers.Length);
+        return new BigInteger(string.Join("", result));
+    }
+
+    private int Greater(int firstNum, int secondNum)
+    {
+        if (firstNum > secondNum)
+        {
+            return firstNum;
+        }
+
+        return secondNum;
+    }
+
+    public void Split<T>(T[] source, int index, out T[] first, out T[] last)
+    {
+        int len2 = source.Length - index;
+        first = new T[index];
+        last = new T[len2];
+        Array.Copy(source, 0, first, 0, index);
+        Array.Copy(source, index, last, 0, len2);
+    }
+
+    public BigInteger Multiplication(BigInteger another)
+    {
+        var firstMultiplier = new BigInteger(string.Join("", _numbers));
+        var result = new BigInteger("");
+        Karatsuba(firstMultiplier, another);
+
+        BigInteger Karatsuba(BigInteger first, BigInteger second)
+        {
+            if (first._numbers.Length == 1 || second._numbers.Length == 1)
+            {
+                var result = Int32.Parse(string.Join("", first._numbers)) *
+                             Int32.Parse(string.Join("", second._numbers));
+                return new BigInteger(result.ToString());
+            }
+
+            var dividedArrLen = Greater(first._numbers.Length, second._numbers.Length) / 2;
+            int[] a1 = new int[dividedArrLen];
+            int[] b1 = new int[dividedArrLen];
+            int[] c1 = new int[dividedArrLen];
+            int[] d1 = new int[dividedArrLen];
+            Split(first._numbers, dividedArrLen, out a1, out b1);
+            Split(second._numbers, dividedArrLen, out c1, out d1);
+            var a = new BigInteger(string.Join("", a1));
+            var b = new BigInteger(string.Join("", b1));
+            var c = new BigInteger(string.Join("", c1));
+            var d = new BigInteger(string.Join("", d1));
+            var bd = Karatsuba(b, d);
+            var ac_bd = Karatsuba(a + b, c + d);
+            var ac = Karatsuba(a, c);
+            var best = ((ac.PowerNum(dividedArrLen * 2)) + (ac_bd - ac - bd)).PowerNum(dividedArrLen) + bd;
+            result = best;
+            return best;
+        }
+
+        return result;
+
+        // var firstArrLen = _numbers.Length;
+        // var secondArrLen = another._numbers.Length;
+        // var a = Int32.Parse(string.Join("", _numbers[0], _numbers[1]));
+        // var b = Int32.Parse(string.Join("", _numbers[2], _numbers[3]));
+        // var c = Int32.Parse(string.Join("", another._numbers[0], another._numbers[1]));
+        // var d = Int32.Parse(string.Join("", another._numbers[2], another._numbers[3]));
+        // var x = Math.Pow(10, firstArrLen / 2) * a + b;
+        // var y = Math.Pow(10, secondArrLen / 2) * c + d;
+        // var result = x * y;
+        // var best = Math.Pow(10, firstArrLen) * a * c + Math.Pow(10, secondArrLen / 2) * (a * d + b * c) + b * d ;
     }
 }
--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,16 +1,16 @@
 ﻿using Task_5;
 
-var binNum = new BigInteger("2000");
-var bigNum2 = new BigInteger("4000");
+
+// ---- SUBTRACTION ----
+var binNum = new BigInteger("46546464564564123");
+var bigNum2 = new BigInteger("81379281731982731");
 var result2 = binNum - bigNum2;
 Console.WriteLine(result2);
-var result = new BigInteger("3123123123") + new BigInteger("10000000132123123123");
-Console.WriteLine(result);
 
-int[] array = new[] {1, 2, 3};
-Console.WriteLine(string.Join("", array[0], array[1]));
+// ---- ADDITION ----
+Console.WriteLine(binNum + bigNum2);
 
-var result3 = new BigInteger("5678").Multiplication(new BigInteger("1234"));
 
-Console.WriteLine(result3);
-
+// ---- Karatsuba ----
+var result5 = new BigInteger("5678").Multiplication(new BigInteger("1234"));
+Console.WriteLine(result5);
